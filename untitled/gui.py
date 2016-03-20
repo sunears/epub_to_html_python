bd20b9f8e084fe0cda8c2dbecd8c079c7392d005
sunears <sunears@gmail.com>
2016-03-20 17:10:42 +0800

    add class HandDicm

--- untitled/gui.py
+++ untitled/gui.py
@@ -1,6 +1,16 @@
 #!/usr/bin/python
 
-import tkinter
-top = tkinter.Tk()
+# import tkinter
+# top = tkinter.Tk()
 # Code to add widgets will go here...
-top.mainloop()
+# top.mainloop()
+# label = tkinter.Label(top,text='Hello World')
+# tkinter.mainloop()
+
+from kivy.app import App
+from kivy.uix.button import Button
+class MyApp(App):
+    def build(self):
+        return Button(text='hello world')
+if __name__=='__main__':
+    MyApp().run()
--- untitled/hand_dicm.py
+++ untitled/hand_dicm.py
@@ -1,3 +1,23 @@
 import sys
+from datetime import datetime
+import time
+
+class HandDicm(object):
+    def __init__(self):
+        pass
+    def __init__(self,dir_name=""):
+        pass
+    def get_now(self):
+        return time.strftime('%Y%m%d_%H%M%S')
+    def include_underline(self,filename):
+        if isinstance(filename,str):
+            if "_" in filename:
+                return True
+        return False
+def test():
+    hd = HandDicm()
+    print(hd.get_now())
+    print(hd.include_underline(hd.get_now()))
 if __name__ == "__main__":
+    test()
     print(sys.argv[0])
