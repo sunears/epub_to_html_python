afb668d69017693c79e1e65dae563870089f831a
sunears <sunears@gmail.com>
2016-03-21 09:39:05 +0800

    测试一下steup and gui

--- untitled/gui.py
+++ untitled/gui.py
@@ -7,10 +7,19 @@
 # label = tkinter.Label(top,text='Hello World')
 # tkinter.mainloop()
 
-from kivy.app import App
-from kivy.uix.button import Button
-class MyApp(App):
-    def build(self):
-        return Button(text='hello world')
-if __name__=='__main__':
-    MyApp().run()
+# from kivy.app import App
+# from kivy.uix.button import Button
+# class MyApp(App):
+#     def build(self):
+#         return Button(text='hello world')
+# if __name__=='__main__':
+#     MyApp().run()
+
+from tkinter import *
+
+frmMain = Tk()
+
+label = Label(frmMain, text="Welcome to py2exe!")
+label.pack()
+
+frmMain.mainloop()
